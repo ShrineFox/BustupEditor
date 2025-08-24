ad72a068378823c8e38f41e151715fab93d820af
ShrineFox <ShrineFox@users.noreply.github.com>
2025-08-24 19:51:05 -0400

    Fix DPI & compiling error

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace BustupEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.Run(new MainForm());
         }
     }
